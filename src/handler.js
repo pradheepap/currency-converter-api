5e59a1d0672ae35d906064f8f06dddd8112c442c
Pradheepa P <pradheepa.p@gmail.com>
2022-03-07 00:55:56 +0800

    DS added for Appsync

--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,15 @@
 
 module.exports.convertCurrency = async (event, context) => {
-
-    console.log(`convertCurrency Request - ${JSON.stringify(event)}`);
+  console.log(`convertCurrency Request - ${JSON.stringify(event)}`);
   console.log(`convertCurrency Context - ${JSON.stringify(context)}`);
-  
+
   return {
-    
-    
-      from: 'USD',
-      to: 'EUR',
-      amount: 1.5,
-      output: 3.4566
-    
-}}
+
+
+    from: 'USD',
+    to: 'EUR',
+    amount: 1.5,
+    output: 3.4566,
+
+  };
+};
